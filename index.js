73ec3d5db1ce7e43c463edf27740bd9d9ec55987
Kazuyuki Honda <hakobera@gmail.com>
2010-10-25 04:28:28 +0900

    Improve UX.  Add send message shortcut  Show sent message log.

---
+++ index.js
@@ -0,0 +1,128 @@
+new function() {
+	var ws = null;
+	var connected = false;
+
+	var serverUrl;
+	var connectionStatus;
+	var sendMessage;
+	
+	var connectButton;
+	var disconnectButton; 
+	var sendButton;
+
+	var open = function() {
+		var url = serverUrl.val();
+		ws = new WebSocket(url);
+		ws.onopen = onOpen;
+		ws.onclose = onClose;
+		ws.onmessage = onMessage;
+		ws.onerror = onError;
+
+		connectionStatus.text('OPENING ...');
+		serverUrl.attr('disabled', 'disabled');
+		connectButton.hide();
+		disconnectButton.show();
+	}
+	
+	var close = function() {
+		if (ws) {
+			console.log('CLOSING ...');
+			ws.close();
+		}
+		connected = false;
+		connectionStatus.text('CLOSED');
+
+		serverUrl.removeAttr('disabled');
+		connectButton.show();
+		disconnectButton.hide();
+		sendMessage.attr('disabled', 'disabled');
+		sendButton.attr('disabled', 'disabled');
+	}
+	
+	var clearLog = function() {
+		$('#messages').html('');
+	}
+	
+	var onOpen = function() {
+		console.log('OPENED: ' + serverUrl.val());
+		connected = true;
+		connectionStatus.text('OPENED');
+		sendMessage.removeAttr('disabled');
+		sendButton.removeAttr('disabled');
+	};
+	
+	var onClose = function() {
+		console.log('CLOSED: ' + serverUrl.val());
+		ws = null;
+	};
+	
+	var onMessage = function(event) {
+		var data = event.data;
+		addMessage(data);
+	};
+	
+	var onError = function(event) {
+		alert(event.data);
+	}
+	
+	var addMessage = function(data, type) {
+		var msg = $('<pre>').text(data);
+		if (type === 'SENT') {
+			msg.addClass('sent');
+		}
+		var messages = $('#messages');
+		messages.append(msg);
+		
+		var msgBox = messages.get(0);
+		while (msgBox.childNodes.length > 1000) {
+			msgBox.removeChild(msgBox.firstChild);
+		}
+		msgBox.scrollTop = msgBox.scrollHeight;
+	}
+
+	WebSocketClient = {
+		init: function() {
+			serverUrl = $('#serverUrl');
+			connectionStatus = $('#connectionStatus');
+			sendMessage = $('#sendMessage');
+			
+			connectButton = $('#connectButton');
+			disconnectButton = $('#disconnectButton'); 
+			sendButton = $('#sendButton');
+			
+			connectButton.click(function(e) {
+				close();
+				open();
+			});
+		
+			disconnectButton.click(function(e) {
+				close();
+			});
+			
+			sendButton.click(function(e) {
+				var msg = $('#sendMessage').val();
+				addMessage(msg, 'SENT');
+				ws.send(msg);
+			});
+			
+			$('#clearMessage').click(function(e) {
+				clearLog();
+			});
+			
+			var isCtrl;
+			sendMessage.keyup(function (e) {
+				if(e.which == 17) isCtrl=false;
+			}).keydown(function (e) {
+				if(e.which == 17) isCtrl=true;
+				if(e.which == 13 && isCtrl == true) {
+					sendButton.click();
+					return false;
+				}
+			});
+		}
+	};
+}
+
+$(function() {
+	WebSocketClient.init();
+});
